c188cd7ea68625b84bb47b009843003672718af7
Hankiii890 <shenderuk4kir@gmail.com>
2024-08-04 15:16:31 +0300

    Adding a reader function

--- ki.py
+++ ki.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import List, Dict
 
 class Publication(ABC):
     "Абстрактный базовый класс для всех публикаций"
@@ -37,22 +38,44 @@ class Newspapers(Publication):
 
 
 class Reader:
-    "Класс читатель, который имеет методы для взятия и возврата публикаций"
+    """Класс читатель, который имеет методы для взятия и возврата публикаций,
+    а так же используем аннотацию типов (typing)
+    """
     def __init__(self, name: str):
         self.name = name
-        self.borrowed_publications = []
-
-    # Методы для взятия и удаления публикации
-    pass
+        self.borrowed_publications: List[Publication] # Список всех публикация, которые взял читатель
 
 
-class Library:
-    "Класс библиотека, который имеет методы для добавления, удаления публикаций и читателей"
+    def borrow(self, publication:Publication):
+        "Функция взятия публикаций юзерами"
+        if publication.aviable:
+            publication.aviable = False # Указываем, что публикация больше не доступна для взятия другими пользователями 
+            self.borrowed_publications.append(publication)
+            return f"Пользователь {publication.name} взял публикацию {publication.__title}"
+        else:
+            return f"Публикация {publication.__title} уже используется!"
+
+
+    def return_publication(self, publication: Publication):
+        """
+        Функция проверки заимствованных публикаций
+        """
+        if publication in self.borrowed_publications:
+            publication.aviable = True 
+            self.borrowed_publications.remove(publication)
+            return f"Пользователь {self.name} вернул публикацию {publication.__title}"
+        else:
+            return f"Пользователь не брал {publication.__title}"
+        
+            
+class Librari:
+    """Класс библиотека, который имеет методы для добавления, удаления публикаций и читателей,
+    а так же используем композицию для испольхования экземпляров других классов
+    """
     def __init__(self):
-        self.publications = [] 
-        self.readers = []
+        self.publications: Dict[int, Publication] = {}
+        self.readers: List[Reader] = []
 
-    # Методы для управления библиотекой 
-    pass 
+    
 
 
